e7b7fc3312623451975743075183b5fbebd5038a
Nimisha <104964276+Nimisha-n-s@users.noreply.github.com>
2023-05-08 14:30:11 +0530

    Internship

---
+++ object.js
@@ -0,0 +1,14 @@
+let person ={
+    nam:"nimisha",
+    age:10,
+    place:"perumbavoor",
+    address:{
+        hname:"abccdf",
+        pname:"jklmnb"
+    }
+    
+
+};
+//console.log(person);
+let me=`my name is ${person.nam} and my place is ${person.place}`;
+console.log(me);
---
+++ stringop.js
@@ -0,0 +1,11 @@
+//string operators
+let fname="Nimisha ";
+let lname="N S";
+let fullname=fname+lname;
+//console.log(fullname);
+let names=`${fname} ${lname}`;
+//console.log(names)
+let n1="amaya";
+let n2="fasila";
+let nn=`${n1} &${n2}`;
+console.log(nn);
